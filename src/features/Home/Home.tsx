ac2a5478e4bce9427b25bf910942b0ee47783bee
Alexander Kung <akung@secretmission.io>
2020-06-08 12:13:06 -0400

    feat: made some style changes to incorporate `react-native-elements` button

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Button } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { StyleSheet, View } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    content: {
+        paddingHorizontal: 20,
+    },
+})
 
 const Home: React.FC = () => {
     return (
-        <SafeAreaView>
-            <Text>Start Here!</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.content}>
+                <Button title="Start here!" />
+            </View>
         </SafeAreaView>
     )
 }
