c37ec93b5e5a3acc8fc1e5799e21736e485128cb
Alexander Kung <akung@secretmission.io>
2020-06-08 22:11:10 -0400

    feat: added `useInterval` helper

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,4 +1,5 @@
 import Axios from 'axios'
+import { useEffect, useRef } from 'react'
 
 export const axiosClient = Axios.create({
     // baseURL: '',
@@ -6,3 +7,25 @@ export const axiosClient = Axios.create({
     //     Authorization: `Bearer ${token}`
     // }
 })
+
+export const useInterval = (callback: () => void, delay: number | null) => {
+    const savedCallback: any = useRef()
+
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback
+    }, [callback])
+
+    // Set up the interval.
+    useEffect(() => {
+        const tick = () => {
+            if (savedCallback.current !== undefined) {
+                savedCallback.current()
+            }
+        }
+        if (delay !== null) {
+            const id = setInterval(tick, delay)
+            return () => clearInterval(id)
+        }
+    }, [delay])
+}
