1b161c83cf2b8e69cc872995d7145bc7ec2c9a94
Alexander Kung <akung@secretmission.io>
2020-06-14 17:18:10 -0400

    feat: added svg transformer

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StyleSheet, View } from 'react-native'
+import { ShoppingBagIcon } from '../../shared/Icons'
 
 const styles = StyleSheet.create({
     container: {
@@ -16,7 +17,7 @@ const Home: React.FC = () => {
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
-                <Button title="Start here!" />
+                <Button title="Start here!" icon={<ShoppingBagIcon width={40} height={40} fill={'#fffbf6'} />} />
             </View>
         </SafeAreaView>
     )
---
+++ src/shared/Icons.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ShoppingBag from '../../assets/icons/shopping-bag.svg'
+import { SvgProps } from 'react-native-svg'
+
+export const ShoppingBagIcon: React.FC<SvgProps> = (props: SvgProps) => <ShoppingBag {...props} />
