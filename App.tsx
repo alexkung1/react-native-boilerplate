a4b2b29c1d9b49307a2aec6e81be095449c0e182
Alexander Kung <akung@secretmission.io>
2020-06-21 22:45:45 -0400

    fix: removed rootnavigator at beginning that was just confusing

--- App.tsx
+++ App.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import RootNavigator from './src/features/RootNavigator'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 // import StorybookUI from './storybook'
 
 const App = (): React.ReactElement => {
-    return (
-        <SafeAreaProvider>
-            <RootNavigator />
-        </SafeAreaProvider>
-    )
+    return <SafeAreaProvider></SafeAreaProvider>
 }
 
 // export default StorybookUI
